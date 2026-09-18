023ea94a10c8a5cebc0f5e9cf8776690231685ed
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-09-18 18:19:59 +0200

    Create init files and the empty PiecewiseLinearFunction class

---
+++ pw_linfunc/piecewise_linear_function.py
@@ -0,0 +1,12 @@
+class PiecewiseLinearFunction:
+    """
+    A class that represents a piecewise linear function defined by a list of
+    tuples.
+    """
+    def __init__(self, point_list: list[tuple]):
+        """
+        Constructor
+        :param list[tuple] point_list: the list of tuples that defines the
+               piecewise linear function
+        """
+        pass
